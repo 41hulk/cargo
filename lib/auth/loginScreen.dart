8143dd8e337c67c5dadfb0589e3e6b6eaf78ff5e
Ntare Guy <gntare2@gmail.com>
2021-09-20 13:44:13 +0200

    initial commit

---
+++ lib/auth/loginScreen.dart
@@ -0,0 +1,145 @@
+import 'package:flutter/cupertino.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatefulWidget {
+  @override
+  _LoginScreenState createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  @override
+  Widget build(BuildContext context) {
+    final logo = Hero(
+      tag: 'hero',
+      child: CircleAvatar(
+        backgroundColor: Colors.transparent,
+        radius: 120.0,
+        child: Image.asset('assets/images/ewawelogo.png'),
+      ),
+    );
+
+    final email = TextFormField(
+      keyboardType: TextInputType.emailAddress,
+      autofocus: false,
+      decoration: InputDecoration(
+        hintText: 'Email',
+        hintStyle: GoogleFonts.poppins(fontSize: 15),
+        contentPadding: EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
+        border: OutlineInputBorder(borderRadius: BorderRadius.circular(10.0)),
+      ),
+    );
+
+    final password = TextFormField(
+      autofocus: false,
+      obscureText: true,
+      decoration: InputDecoration(
+        hintText: 'Password',
+        hintStyle: GoogleFonts.poppins(fontSize: 15),
+        contentPadding: EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
+        border: OutlineInputBorder(borderRadius: BorderRadius.circular(10.0)),
+      ),
+    );
+    final forgotpassword = Container(
+      padding: EdgeInsets.only(top: 18),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: <Widget>[
+          // Text('Don\'t have an Account? '),
+          InkWell(
+            onTap: () async {
+              // Navigator.push(
+              //   context,
+              //   CupertinoPageRoute(
+              //     builder: (context) => SetPhoneScreen(),
+              //   ),
+              // );
+            },
+            child: new Text(
+              "Forgot password ?",
+              style: GoogleFonts.poppins(
+                  color: Colors.lightGreen, fontWeight: FontWeight.normal),
+            ),
+          ),
+        ],
+      ),
+    );
+
+    final loginButton = Padding(
+      padding: EdgeInsets.symmetric(vertical: 19.0),
+      child: RaisedButton(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(18),
+        ),
+        onPressed: () {
+          // // Navigator.of(context).pushNamed(HomePage.tag);
+          // Navigator.of(context).pushAndRemoveUntil(
+          //     MaterialPageRoute(
+          //         builder: (BuildContext context) => DashboardScreen()),
+          //     (Route<dynamic> route) => false);
+        },
+        padding: EdgeInsets.all(12),
+        color: Colors.grey,
+        child: Text('LOG IN',
+            style: GoogleFonts.poppins(
+                fontSize: 20,
+                color: Colors.white,
+                fontWeight: FontWeight.w500)),
+      ),
+    );
+
+    final signup = Container(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Text(
+            'Don\'t have an Account? ',
+            style: GoogleFonts.poppins(fontWeight: FontWeight.normal),
+          ),
+          InkWell(
+            onTap: () async {
+              // Navigator.push(
+              //   context,
+              //   CupertinoPageRoute(
+              //     builder: (context) => SignUpScreen(),
+              //   ),
+              // );
+            },
+            child: new Text("Sign up",
+                style: GoogleFonts.poppins(color: Colors.lightGreenAccent)),
+          ),
+        ],
+      ),
+    );
+
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+        child: SingleChildScrollView(
+          physics: BouncingScrollPhysics(),
+          child: Container(
+            padding: EdgeInsets.only(
+              left: 20,
+              right: 20,
+            ),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                logo,
+                SizedBox(height: 40.0),
+                email,
+                SizedBox(height: 15.0),
+                password,
+                forgotpassword,
+                SizedBox(height: 12.0),
+                loginButton,
+                signup
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:animated_splash_screen/animated_splash_screen.dart';
+import 'package:cargo/auth/loginScreen.dart';
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MyApp(),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Ewawe Pay',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: AnimatedSplashScreen(
+        duration: 3000,
+        splashIconSize: 300,
+        splashTransition: SplashTransition.fadeTransition,
+        backgroundColor: Colors.white,
+        splash: Image.asset('assets/images/cargologo.jpeg'),
+        nextScreen: LoginScreen(),
+      ),
+    );
+  }
+}
